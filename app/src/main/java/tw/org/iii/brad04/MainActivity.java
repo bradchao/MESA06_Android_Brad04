c1a5da71c6359a7eabb243b9cfba62bfdd786c57
Brad Chao <brad@brad.tw>
2016-11-16 10:25:23 +0800

    v3

--- app/src/main/java/tw/org/iii/brad04/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad04/MainActivity.java
@@ -1,17 +1,24 @@
 package tw.org.iii.brad04;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button test1, test2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v("brad","onCreate");
+
+        test1 = (Button)findViewById(R.id.test1);
+        test2 = (Button)findViewById(R.id.test2);
+
     }
 
     @Override
@@ -51,6 +58,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test1(View view){
-
+        Intent it = new Intent(this, Page2Activity.class);
+        startActivity(it);
+        Log.v("brad", "test1 return");
     }
+
+
+
 }
